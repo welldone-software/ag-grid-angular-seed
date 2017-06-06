429788db66f48380fc6d8592384e50f08fa35b67
Sean Landsman <sean@thelandsmans.com>
2017-06-06 15:12:53 +0100

    Add angular cli

---
+++ ag-grid-cli/src/app/my-grid-application/my-grid-application.component.ts
@@ -0,0 +1,35 @@
+import {Component} from "@angular/core";
+import {GridOptions} from "ag-grid";
+import {RedComponentComponent} from "../red-component/red-component.component";
+
+@Component({
+    selector: 'app-my-grid-application',
+    templateUrl: './my-grid-application.component.html'
+})
+export class MyGridApplicationComponent {
+    private gridOptions: GridOptions;
+
+    constructor() {
+        this.gridOptions = {};
+        this.gridOptions.columnDefs = [
+            {
+                headerName: "ID",
+                field: "id",
+                width: 100
+            },
+            {
+                headerName: "Value",
+                field: "value",
+                cellRendererFramework: RedComponentComponent,
+                width: 100
+            },
+
+        ];
+        this.gridOptions.rowData = [
+            {id: 5, value: 10},
+            {id: 10, value: 15},
+            {id: 15, value: 20}
+        ]
+    }
+}
+
